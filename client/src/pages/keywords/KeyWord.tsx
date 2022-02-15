512982f5490a37ddab7f180affb73831f6280fb4
SimpleLogic420 <simplelogic420@gmail.com>
2022-02-15 20:40:56 +0200

    chnaged substring size of date in notification

--- client/src/pages/keywords/KeyWord.tsx
+++ client/src/pages/keywords/KeyWord.tsx
@@ -112,7 +112,7 @@ function KeyWord({
   function formatDateTo_DD_MM_YYYY(date: Date) {
     //duplicated for lack of time ,(barchart)
     let dateStr = date.toString();
-    let date1 = dateStr.substring(4, 15);
+    let date1 = dateStr.substring(4, 16);
     const rightFormatDate =
       date1.split(" ")[1] +
       " " +
